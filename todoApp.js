6ce8de7db1d4f46d24179a2b7696a209d8adce34
rkjdev <rk@rkpc.local>
2025-05-08 15:07:38 +0530

    added a taskDisplay area. New tasks will now have a delete button to delete it from the taskDisplay

--- todoApp.js
+++ todoApp.js
@@ -3,15 +3,36 @@ window.addEventListener ("load", init, false);
 // dom handles
 var handle_input = null; 
 var handle_inputButton = null;
+var handle_taskDisplay = null;
 
 function init() {
-	handle_input = document.getElementById ("todoApp_input");
-	handle_inputButton = document.getElementById ("todoApp_inputButton");
-	handle_inputButton.addEventListener ("click", inputButtonManager, false);
+	handle_input = document.getElementById ('todoApp_input');
+	handle_inputButton = document.getElementById ('todoApp_inputButton');
+	handle_taskDisplay = document.getElementById ('todoApp_taskDisplay');
+	handle_inputButton.addEventListener ('click', inputButtonManager, false);
 }
 
-function inputButtonManager() {
+function inputButtonManager (event) {
 	var inputText = handle_input.value;
-	console.log (inputText);
+	addTaskElementToTaskDisplay (inputText);
 }
 
+function addTaskElementToTaskDisplay (inputText) {
+	var taskElement = document.createElement ('li');
+	taskElement.textContent = inputText;
+	addDeleteButtonToTaskElement (taskElement);
+	handle_taskDisplay.appendChild (taskElement);
+}
+
+function addDeleteButtonToTaskElement (taskElement) {
+	var taskDeleteButton = document.createElement ('button');
+	taskDeleteButton.textContent = 'delete';
+	taskDeleteButton.addEventListener ('click', taskDeleteButtonManager, false);
+	taskElement.appendChild (taskDeleteButton);
+}
+
+function taskDeleteButtonManager (event) {
+	var taskDeleteButton = event.target;
+	var taskElement = taskDeleteButton.parentNode;
+	taskElement.remove();
+}
